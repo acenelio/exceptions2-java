1c3fb13e15610e2bcfb666b93a269279bb5e41d6
Nelio Alves <acenelio@gmail.com>
2018-07-20 08:30:26 -0300

    Initial implementation

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		try {
+			acc.withdraw(amount);
+			System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+}
